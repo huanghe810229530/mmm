84333ae2d1db2bc6acb389265b85fb8b47779548
River <810229530@qq.com>
2017-11-16 16:30:31 +0800

    ikk
    llkk

---
+++ HuaXiaTests/HuaXiaTests.swift
@@ -0,0 +1,36 @@
+//
+//  HuaXiaTests.swift
+//  HuaXiaTests
+//
+//  Created by haunghe-pc on 2017/11/16.
+//  Copyright © 2017年 HCFDATA. All rights reserved.
+//
+
+import XCTest
+@testable import HuaXia
+
+class HuaXiaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,13 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, ‘9.0’
+use_frameworks!
+
+target 'HuaXia' do
+    pod 'Alamofire', '~> 4.5'
+    pod 'IQKeyboardManager'
+    pod 'SwiftyJSON'
+    pod 'SnapKit', '~> 4.0.0'
+    pod 'RxSwift',    '~> 4.0'
+    pod 'RxCocoa',    '~> 4.0'
+    pod 'XCGLogger', '~> 6.0.1'
+end
